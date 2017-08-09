87c8decf1d3bf1817f97d14ccc8b5573c619536f
xiaoxiaoqingyi <lideyi333@163.com>
2017-08-09 11:15:05 +0800

    添加 ffmpeg 库

--- app/src/main/java/com/opensource/ffmpeg_android_video_decoder/MainActivity.java
+++ app/src/main/java/com/opensource/ffmpeg_android_video_decoder/MainActivity.java
@@ -6,10 +6,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,4 +22,45 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+
+    /**
+     * 打开视频
+     * @param absFileName
+     * @return
+     */
+    public native String  OpenVideo(String absFileName);
+
+    /**
+     *
+     * @param time_sec 开始的时间
+     * @param fps  帧数的间隔
+     * @return
+     */
+    public native boolean SetBeginning(int time_sec, int fps);
+
+    /**
+     *
+     * @param filePath 文件保存地址
+     * @param interval 1秒钟抽的帧数
+     * @return
+     */
+    public native boolean SaveAFrame(String filePath, int interval);
+    public native boolean CloseVideo();
+
+    //载入动态库
+    static {
+        System.loadLibrary("avcodec-57");
+        System.loadLibrary("avdevice-57");
+        System.loadLibrary("avfilter-6");
+        System.loadLibrary("avformat-57");
+        System.loadLibrary("avutil-55");
+        System.loadLibrary("postproc-54");
+        System.loadLibrary("swresample-2");
+        System.loadLibrary("swscale-4");
+        System.loadLibrary("captureframe");   //即Android.mk文件#Program下的LOCAL_MODULE
+
+    }
+
+
 }
